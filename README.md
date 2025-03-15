9d099f828c7ac4369b6af04dc723a80091b46a23
Ko-s-y <100200496+Ko-s-y@users.noreply.github.com>
2025-03-15 10:31:45 +0000

    feat: 指定した座標を赤枠で加工

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 iPadしか手元にない時のCodespacesで手っ取り早く検証する時用
 
 ```
-$ pip install flask pyngrok
-$ pip install python-dotenv
+$ pip install flask pyngrok python-dotenv pillow
 $ python main.py
 ```
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
+from io import BytesIO
 from dotenv import load_dotenv
 from flask import Flask, request, render_template_string, send_file
 from pyngrok import ngrok
+from PIL import Image, ImageDraw
 
 # .envファイルから環境変数を読み込む
 load_dotenv()
@@ -26,14 +28,13 @@ def index():
         if 'image_file' in request.files:
             image = request.files['image_file']
             if image:
-                # Colabセッション内に画像を保存(セッション切れで消えます)
+                # layout.png として保存 (セッション切れでファイル消える点に注意)
                 filename = "layout.png"
                 image.save(filename)
                 uploaded_image_filename = filename
 
     # HTMLテンプレート（画像があれば表示してクリック取得）
-    # 画像表示部分で直接 "layout.png" にアクセスするのではなく、
-    # "/uploaded_image" ルートを叩くようにする。
+    # 画像表示は "/uploaded_image" (下記ルート) を経由して返す
     html_template = '''
 <html>
 <head>
@@ -107,31 +108,61 @@ def index():
 @app.route('/uploaded_image')
 def uploaded_image():
     """
-    アップロードした画像を返すルート。
-    ここから send_file で layout.png を返すことで、リンク切れしなくなる。
+    アップロードした画像に、登録された座標を赤枠で描画して返す。
     """
-    global uploaded_image_filename
+    global uploaded_image_filename, table_data
+
     if not uploaded_image_filename:
         return "No image uploaded."
-    return send_file(uploaded_image_filename, mimetype='image/png')
+
+    # 1) 画像ファイルをPILで開く
+    img = Image.open(uploaded_image_filename)
+    draw = ImageDraw.Draw(img)
+
+    # 2) table_data の各テーブル(x, y)に赤枠を描画
+    #    ここでは中心(x, y)を囲む 20x20px の四角を例にしています
+    #    (x-10, y-10) ～ (x+10, y+10) を囲む
+    box_size = 10
+    for x_str, y_str, table_num in table_data:
+        x = float(x_str)
+        y = float(y_str)
+        left = x - box_size
+        top = y - box_size
+        right = x + box_size
+        bottom = y + box_size
+
+        # outline='red' -> 赤枠, width=3 -> 枠線太さ3px
+        draw.rectangle([left, top, right, bottom], outline='red', width=3)
+
+    # 3) 書き込んだイメージをバイナリとして返す
+    img_io = BytesIO()
+    img.save(img_io, 'PNG')
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/png')
 
 @app.route('/save_coordinate', methods=['POST'])
 def save_coordinate():
+    """
+    テーブルの座標・番号を table_data に追加保存する
+    """
     global table_data
     x = request.form.get("x")
     y = request.form.get("y")
-    tableNum = request.form.get("tableNum")
+    table_num = request.form.get("tableNum")
 
-    table_data.append((x, y, tableNum))
+    table_data.append((x, y, table_num))
 
     return f"""
-    <p>登録しました: テーブル番号 <b>{tableNum}</b> (x={x}, y={y})</p>
+    <p>登録しました: テーブル番号 <b>{table_num}</b> (x={x}, y={y})</p>
     <p><a href='/view_tables'>登録情報を見る</a></p>
     <p><a href='/'>トップへ戻る</a></p>
     """
 
 @app.route('/view_tables')
 def view_tables():
+    """
+    登録されているテーブル情報一覧を表示
+    """
     global table_data
     html = "<h2>登録されたテーブル情報</h2>"
     for idx, (x, y, tnum) in enumerate(table_data):
